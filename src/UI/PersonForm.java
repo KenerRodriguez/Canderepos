dc61d76ebcc75fa072d1077d6349e33f59472c19
KenerRodriguez <canderodriguezkener20@gmail.com>
2022-07-28 01:12:00 -0600

    SE CREA LOGICA DEL FORMULARIO DE DATOS DE PERSONA

--- src/UI/PersonForm.java
+++ src/UI/PersonForm.java
@@ -1,5 +1,12 @@
 package UI;
 
 public class PersonForm {
+	
+	public void requiereData(){
+		
+		System.out.println("INGRESA EL PRIMER NOMBRE");
+		System.out.println("INGRESA EL SEGUNDO NOMBRE");
+		System.out.println("INGRESA EL PRIMER APELLIDO");
+	}
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,14 @@
 package main;
 
+import UI.PersonForm;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo desde java enviado a GitHub");
+		
+		PersonForm form = new PersonForm();
+		form.requiereData();
 
 	}
 
